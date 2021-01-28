3e6766ef4dfbe0e6a7cfab146c2fa87faf52d4f7
Chiramium <chiramium@gmail.com>
2021-01-28 18:39:39 +0900

    [ADD] initial commit.

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container-fluid bg-dark">
+    <div class="container">
+      <nav class="navbar navbar-dark">
+        <span class="navbar-brand h1">SimpleBBS</span>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ resources/js/components/ThreadComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <table class="table table-hover">
+      <thead class="thread-light">
+        <tr>
+          <th scope="col">Title</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Name</td>
+        </tr>
+        <tr>
+          <td colspan="2">descriptiondescriptiondescription</td>
+        </tr>
+      </tbody>
+      <tbody>
+        <tr>
+          <th scope="row">2</th>
+          <td>Namae</td>
+        </tr>
+        <tr>
+          <td colspan="2">shousaishousaishousai</td>
+        </tr>
+      </tbody>
+      <tbody>
+        <tr>
+          <th scope="row">3</th>
+          <td>Mingzi</td>
+        </tr>
+        <tr>
+          <td colspan="2">shenmeshenmeshenmeshenme</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
